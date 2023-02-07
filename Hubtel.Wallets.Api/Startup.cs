fa1b2b724ecb7b943f40422793cc78d5c77c695c
Edward-Kwabena-Twumasi <createdliving1000@gmail.com>
2023-02-07 18:28:18 +0000

    project entry point

---
+++ Hubtel.Wallets.Api/Startup.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Hubtel.Wallets.Api.Converters;
+using Hubtel.Wallets.Api.Services;
+using Hubtel.Wallets.Api.Models;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hubtel.Wallets.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddControllers()
+            .AddJsonOptions(options => {
+                options.JsonSerializerOptions.Converters.Add(new WalletConverter());
+            });
+
+            services.AddDbContext<WalletDbContext>(opt =>
+            opt.UseInMemoryDatabase("Wallets"));
+
+            services.AddScoped<IWalletService, WalletService>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+        }
+    }
+}
